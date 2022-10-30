fbd983681f2a68739dfa69f3e0d49a148470f6cf
Mike Dial <michael.dial.4@gmail.com>
2022-10-30 12:33:31 -0400

    bucket policy yut

--- stacks/Uploads.ts
+++ stacks/Uploads.ts
@@ -1,5 +1,6 @@
 import { Bucket, Config, StackContext } from "@serverless-stack/resources";
 import { RemovalPolicy } from "aws-cdk-lib";
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 export function Uploads({ stack }: StackContext) {
   const bucket = new Bucket(stack, "attachments", {
@@ -10,7 +11,38 @@ export function Uploads({ stack }: StackContext) {
       },
     },
   });
-
+  bucket.attachPermissions(
+    [
+      new iam.PolicyStatement({
+        effect: iam.Effect.DENY,
+        principals: [
+          new iam.ServicePrincipal("*")
+        ],
+        actions: ['s3:PutObject'],
+        resources: [
+          `${bucket.bucketArn}/*.png`,
+          `${bucket.bucketArn}/*.gif`,
+          `${bucket.bucketArn}/*.jpeg`,
+          `${bucket.bucketArn}/*.bmp`,
+          `${bucket.bucketArn}/*.csv`,
+          `${bucket.bucketArn}/*.doc`,
+          `${bucket.bucketArn}/*.docx`,
+          `${bucket.bucketArn}/*.odp`,
+          `${bucket.bucketArn}/*.ods`,
+          `${bucket.bucketArn}/*.odt`,
+          `${bucket.bucketArn}/*.pdf`,
+          `${bucket.bucketArn}/*.ppt`,
+          `${bucket.bucketArn}/*.pptx`,
+          `${bucket.bucketArn}/*.rtf`,
+          `${bucket.bucketArn}/*.tif`,
+          `${bucket.bucketArn}/*.tiff`,
+          `${bucket.bucketArn}/*.txt`,
+          `${bucket.bucketArn}/*.xls`,
+          `${bucket.bucketArn}/*.xlsx`,
+        ],
+      }),
+    ]
+  );
   return {
     bucket,
   };
